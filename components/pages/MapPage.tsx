c98470bd2ba295365e3ad9560c5843bfceeef8b2
Lee Yujin <yuwls1102@gmail.com>
2026-02-13 17:37:37 +0900

    refactor(map): remove obsolete locationLabel prop from Sidebar usage

--- components/pages/MapPage.tsx
+++ components/pages/MapPage.tsx
@@ -2,14 +2,11 @@
 
 import NaverMapView from "@/components/features/map/NaverMapView";
 import { Sidebar } from "../shared/layout/Sidebar";
-import { useUserLocationLabel } from "@/lib/hooks/useUserLocationLabel";
 
 export default function MapPage() {
-  const locationLabel = useUserLocationLabel();
-
   return (
     <main className="h-dvh w-dvw flex">
-      <Sidebar locationLabel={locationLabel} />
+      <Sidebar />
       <div className="flex-1 min-w-0">
         <NaverMapView />
       </div>
