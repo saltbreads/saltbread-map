7e16f6b78aedaea5b769b100702f752cd3bd6c97
Lee Yujin <yuwls1102@gmail.com>
2026-03-14 11:37:34 +0900

    feat: 가게 데이터 조회 React Query 훅 추가 및 Sidebar UI 연동

---
+++ lib/queries/usePhotoHighlights.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPhotoHighlights } from "../api/shops";
+
+export function usePhotoHighlights(shopId: string, open: boolean) {
+  return useQuery({
+    queryKey: ["photo-highlights", shopId],
+    queryFn: () => getPhotoHighlights(shopId),
+    enabled: open && !!shopId,
+  });
+}
